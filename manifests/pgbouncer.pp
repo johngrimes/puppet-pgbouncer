edd08a348e9ec2d4ebcb8999bf4008de0d8cc987
John Grimes <john@smallspark.com.au>
2014-09-11 13:23:16 +1000

    Initial commit

---
+++ manifests/pgbouncer.pp
@@ -0,0 +1,51 @@
+class pgbouncer(
+    $db_user,
+    $db_password
+  ) {
+  package { 'pgbouncer': ensure => present }
+
+  service { 'pgbouncer':
+    ensure     => running,
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => true,
+    require    => [
+      Package['pgbouncer'],
+      File['pgbouncer.ini'],
+      File['userlist.txt'],
+      File['/etc/default/pgbouncer']
+    ]
+  }
+
+  file { 'pgbouncer.ini':
+    path    => '/etc/pgbouncer/pgbouncer.ini',
+    ensure  => file,
+    content => template('pgbouncer/pgbouncer.ini.erb'),
+    owner   => 'postgres',
+    group   => 'postgres',
+    mode    => '640',
+    require => Package['pgbouncer'],
+    notify  => Service['pgbouncer']
+  }
+
+  file { 'userlist.txt':
+    path    => '/etc/pgbouncer/userlist.txt',
+    ensure  => file,
+    content => template('pgbouncer/userlist.txt.erb'),
+    owner   => 'postgres',
+    group   => 'postgres',
+    mode    => '640',
+    require => Package['pgbouncer'],
+    notify  => Service['pgbouncer']
+  }
+
+  file { '/etc/default/pgbouncer':
+    ensure  => file,
+    source  => 'puppet:///modules/pgbouncer/pgbouncer',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '644',
+    require => Package['pgbouncer'],
+    notify  => Service['pgbouncer']
+  }
+}
